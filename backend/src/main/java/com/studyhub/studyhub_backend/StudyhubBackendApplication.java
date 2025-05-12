c8e5152c43721b87f9ae0be6f52610dd6ac87aec
SeongCheol Jung <60413291+jsc5023@users.noreply.github.com>
2025-05-13 01:24:00 +0900

    init: 초기 backend 설정

---
+++ backend/src/main/java/com/studyhub/studyhub_backend/StudyhubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.studyhub.studyhub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyhubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyhubBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/studyhub/studyhub_backend/StudyhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyhub.studyhub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyhubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
